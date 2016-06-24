e569fc1cfda92e8149456c0b3040f89ca84211ac
CKGrafico <quique@ckgrafico.com>
2016-06-24 14:34:19 +0200

    add events

--- src/modules/editor/components/form/form.js
+++ src/modules/editor/components/form/form.js
@@ -1,7 +1,23 @@
 (function(global, angular) {
     'use strict';
     
-    function FormController() {
+    function FormController(calendarService) {
+        this.information = this.information || {};
+
+        this.close = function() {
+            this.show = false;
+        };
+
+        this.clean = function() {
+            this.information = {};
+        };
+
+        this.save = function () {
+            this.information.id = this.month + '-' + this.day + '' + Date.now();
+            calendarService.add(this.month, this.day, this.information);
+            this.close();
+            this.clean();
+        };
     }
 
     angular.module('app.editor')
@@ -10,7 +26,8 @@
             bindings: {
                 month: '=',
                 day: '=',
-                show: '='
+                show: '=',
+                information: '<'
             },
             templateUrl: 'modules/editor/components/form/form.html'
         });
